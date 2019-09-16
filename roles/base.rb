b914392973ce3310678fa4e099c53cda06cb261e
root <root@ip-172-31-23-108.us-east-2.compute.internal>
2019-09-16 03:49:32 +0000

    commiting chef

--- roles/base.rb
+++ roles/base.rb
@@ -1,3 +1,3 @@
 name "base"
 description "this contains therecipies that should run on all nodes"
-run_list "recipe[localusers]"
+run_list "recipe[localusers]","recipe[chef-client::delete_validation]","recipe[chef-client::cron]","recipe[chef-client::default]"
